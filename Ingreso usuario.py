4968aaeea99e6e4a1ee157e0d1b341693c900ade
pablotyg <pablo.tyg@gmail.com>
2022-10-31 21:46:01 -0300

    intento fallido de tkinter para ventana

---
+++ Ingreso usuario.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+ventana=tk.Tk()
+ventana.title("Ventana Login")
+
+#Ancho x Alto
+ventana.geometry('380x300')
+ventana.configure(background='dark turquoise')
+
+
+ventana.mainloop()
